d037fc629591e32b3118acf99827b6b0cb9d62db
Akash Anand Maurya <akashanandmaurya@gmail.com>
2025-09-10 01:54:34 +0530

    Implement standardized API response and error handling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import connectDB  from "../db/index.js"
 dotenv.config({
     path:'./env'
 })
-connectDB();
+connectDB()
+.then(
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    }),
+
+    app.on("error",(error)=>{
+        console.log("error",error);
+        throw error
+    })
+)
+.catch((err)=>{
+    conlos.log("mongooDB connection failed",err);
+})
 
 
 
---
+++ utils/asyncrhandler.js
@@ -0,0 +1,26 @@
+// const asynchandler = (requestHandler )=>{
+//     (req, res, next)=>{
+//         Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+//     }
+// }
+
+
+
+
+// !const asynchandler = (func)=>{}
+// * const asynchandler = (func)=>{()=>{}}
+/** * const asynchandler = (func)=>()=>{}
+const asynchandler = (func)=>async()=>{}*/
+const asynchandler = (func)=>async(req, res, next)=>{
+    try {
+        await func(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message:err.message
+        })
+    }
+} 
+
+export {asynchandler}
+  
