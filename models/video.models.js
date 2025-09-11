8bcfb85ae6b2449ed2de7afeb458f2925c876443
Akash Anand Maurya <akashanandmaurya@gmail.com>
2025-09-11 17:40:01 +0530

    userModels and videomodels by using bcrypt,json-aggrate, jsonWebtoken

---
+++ models/video.models.js
@@ -0,0 +1,41 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema= new Schema({
+    videoFile:{
+        type:String,
+        required:true
+    },
+    thumbNail:{
+        type:String,
+        required:true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    duration: {
+        type:Number, //!clowdinary url
+        required:true
+    },
+    view:{
+        type:Number,
+        defualt:0
+    },
+    isPublished:{
+        type:Boolean,
+        defualt:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+},
+{
+    timestamps: true
+}
+)
+  
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",videoSchema)
